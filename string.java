9da94b80897bfe6dbf4bf53cdc69e380eb891c83
Sonam Kumari <aspritualgirl@gmail.com>
2025-10-21 08:41:16 +0530

    strings

---
+++ string.java
@@ -0,0 +1,11 @@
+public class string {
+    public static void main(String[] args) {
+        //String name = new String("sonam");
+        //this works same as above
+        String name = "sonam";
+        System.out.println("hello" + name);
+        System.out.println(name.charAt(2));
+        System.out.println(name.concat("kumari"));
+        //we write string in capital because it's a class
+    }
+}

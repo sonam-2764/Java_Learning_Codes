50d3920848617a123d19fb786461fb9caaf90951
Sonam Kumari <aspritualgirl@gmail.com>
2025-10-22 09:07:32 +0530

    static variable

---
+++ static_variable.java
@@ -0,0 +1,30 @@
+class Mobile{
+    String brand;
+    int price;
+    String network;
+    String name;
+    //if we do "static string name;" and then if obj1.name = "phone" this will change all the value of name either in obj1 or obj2
+    public void show()
+    {
+        System.out.println(brand + " : " + price + " : "+ name);
+    }
+}
+public class static_variable {
+    public static void main(String[] args) {
+        Mobile obj1 = new Mobile();
+        obj1.brand = "apple";
+        obj1.price= 1700;
+        obj1.name = "smart phone";
+        //Mobile.name="smart phone";//if we use static variable then we will call it using class name
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "samsung";
+        obj2.price= 5432;
+        obj2.name = "foldable phone";
+
+        obj1.show();
+        obj2.show();
+
+
+    }
+}

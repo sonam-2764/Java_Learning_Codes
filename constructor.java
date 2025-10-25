8b4f607fa1e5bf00eeb998de2fc5f7736e967000
Sonam Kumari <aspritualgirl@gmail.com>
2025-10-25 19:19:51 +0530

    default & parametrisefd constructor

--- constructor.java
+++ constructor.java
@@ -3,3 +3,38 @@
 //constructor is a method and has ()
 //it doesn't return anything so it does not have return type like int void etc...
 //if we create 2 object constructor will be called 2 times 
+class Human{
+       private int age;
+    private String name = "sonam";
+  
+    public Human(){//defaukt constructor
+        age = 12;
+        name = "sonam";
+    }
+
+    public Human(int age, String name){//parametrised constructor
+        this.age = age;
+        this.name= name;
+
+    }
+    public int getAge(){//method getAge
+        return age;
+    }
+
+    //if we want to get data from user 
+    public void SetAge(int a){
+            age=a;
+    }
+    public String getName(){//method getName
+        return name;
+    }
+}
+
+public class constructor{
+    public static void main(String[] args) {
+        Human obj = new Human();
+        //obj.SetAge(30);
+        System.out.println(obj.getName() + " : " + obj.getAge());
+        //this keyword refers to the curent object which is call the age(variable)
+    }
+}

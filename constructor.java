4d66343741e50f847e9e286579776642ec2c5843
Sonam Kumari <aspritualgirl@gmail.com>
2025-10-25 19:00:55 +0530

    constructor

---
+++ constructor.java
@@ -0,0 +1,5 @@
+//constructor should have same name as the class name
+//it automatically gets called whhen object is called 
+//constructor is a method and has ()
+//it doesn't return anything so it does not have return type like int void etc...
+//if we create 2 object constructor will be called 2 times 

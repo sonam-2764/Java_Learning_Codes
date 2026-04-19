13b48dff5a3b5597b11e1ccfcd0612fcf00d6795
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-19 23:02:33 +0530

    update

--- oop_java/introduction.java
+++ oop_java/introduction.java
@@ -5,11 +5,11 @@ public class introduction {
     int roll_no;
     public static void main(String[] args) {
         //System.out.println(name);//can't be accessed as we need object
-        introduction obj = new introduction();//new allocates a new space in memory for the object we are creating
+        introduction obj = new introduction();//new word allocates a new space in memory for the object we are creating
         obj.name = "Sonam";
         System.out.println(obj.name);
     }
 }
-// class is the blueprint of abject
+// class is the blueprint of object
 //object refers to behaviour or properties
-//oop is important to store large amount of data ,code reusability, secure
+//oop is important to store large amount of data ,code reusability, secure , 

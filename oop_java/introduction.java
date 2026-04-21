228dd3810f8bee0b9b5f6b2017caeb467830e89a
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-21 15:45:09 +0530

    object

--- oop_java/introduction.java
+++ oop_java/introduction.java
@@ -3,11 +3,21 @@ package oop_java;
 public class introduction {
     String name;
     int roll_no;
+    public void printdata(){
+        System.out.println(name);
+        System.out.println(roll_no);
+    }
     public static void main(String[] args) {
         //System.out.println(name);//can't be accessed as we need object
         introduction obj = new introduction();//new word allocates a new space in memory for the object we are creating
         obj.name = "Sonam";
+        obj.roll_no=50;
         System.out.println(obj.name);
+        obj.printdata();
+
+        introduction obj1 = new introduction();
+        obj1.name= "Tanu";
+        obj1.printdata();
     }
 }
 // class is the blueprint of object

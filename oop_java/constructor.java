c5344a7086b3403a61c5ce21977da3af8353acf4
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-24 14:28:26 +0530

    types of constructor

--- oop_java/constructor.java
+++ oop_java/constructor.java
@@ -7,11 +7,22 @@ class Student{
     Student(int id, int age){
         this.id = id;
         this.age = age;
-        System.out.println("hello");
+        //System.out.println("hello");
+        System.out.println("Parametrized constructor");
+    }
+
+    Student(int id, String name, int age){
+        System.out.println("Parametrized constructor 2");
+    }
+
+    Student(){
+        System.out.println("Default constructor");
     }
 }
 public class constructor {
     public static void main(String[] args) {
+        Student obj1 = new Student();
+        Student obj2 = new Student(10, "sonam", 20);1
         Student obj = new Student(4, 20);//object creation
         // this will run automatically without calling the function Student
         //because constructor gets automatically called just the method name should be same as class name

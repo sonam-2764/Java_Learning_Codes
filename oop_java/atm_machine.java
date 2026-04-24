d52c8d414e4632f4f1e6fd453b3bc8eba4328131
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-24 13:48:59 +0530

    atm

---
+++ oop_java/atm_machine.java
@@ -0,0 +1,56 @@
+package oop_java;
+import java.util.*;
+class ATM{
+    float Balance; 
+    int PIN = 5674;
+    public void checkpin(){
+        System.out.println("Enter your PIN");
+        Scanner sc = new Scanner(System.in);
+        int enterpin = sc.nextInt();
+        if(PIN == enterpin){
+            menu();
+        }
+        else{ System.out.println("Enter a valid pin");
+
+        }
+    }
+        public void menu(){
+            System.out.println("Eneter your choice");
+            System.out.println("1. Check your balance");
+            System.out.println("2. Withdraw Money");
+            System.out.println("3. Deposit money");
+            System.out.println("4. Exit");
+
+            Scanner sc = new Scanner(System.in);
+            int opt = sc.nextInt();
+
+            if(opt == 1){
+                checkBalance();
+            }
+            else if(opt == 2){
+                withdrawMoney();
+            }
+            else if(opt == 3){
+                depositMoney();
+            }
+            else if(opt == 4){
+                break;
+            }
+            else {
+                System.out.println("Enter a valid choice");
+            }
+        }
+    
+        public void checkBalance(){
+            System.out.println("Balance is "+ Balance);
+            menu();
+        }
+        
+    }
+}
+
+public class atm_machine {
+    public static void main(String[] args) {
+        
+    }
+}

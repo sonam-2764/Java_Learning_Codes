014c456aead15c7476bf20557b8e01ca19644d45
Sonam Kumari <aspritualgirl@gmail.com>
2026-04-27 22:35:07 +0530

    theory

---
+++ oop_java/inheritance.java
@@ -0,0 +1,8 @@
+package oop_java;
+// inheritance is the property of acquiring properties of one class into another
+//use cases
+//method overriding
+//reuse the code and reduce duplication
+public class inheritance {
+    
+}

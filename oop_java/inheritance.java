4169059405ffe00f4992aa2ce1d76a8615be3893
Sonam Kumari <aspritualgirl@gmail.com>
2026-05-02 22:39:23 +0530

    change

--- oop_java/inheritance.java
+++ oop_java/inheritance.java
@@ -1,8 +1,8 @@
 package oop_java;
 // inheritance is the property of acquiring properties of one class into another
-//use cases
+//use cases:-
 //method overriding
-//reuse the code and reduce duplication
+//reuse the code and reduce duplication 
 public class inheritance {
     
 }

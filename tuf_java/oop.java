1cddfa632f8e9a6056a7b4d429df4a137a0d7ff8
Sonam Kumari <aspritualgirl@gmail.com>
2026-09-15 14:26:12 +0530

    oops

--- tuf_java/oop.java
+++ tuf_java/oop.java
@@ -1,4 +1,5 @@
 package tuf_java;
+import java.util.*;
 // class Test{
 //     public static void printTest(){
 //         System.out.println("hello");
@@ -11,22 +12,50 @@ package tuf_java;
 //     }
 // }
 
-class Test{
-    int age;
-    public void assignAge(int num){
-        age = num;
+
+
+
+
+
+
+
+// class Test{
+//     int age;
+//     public void assignAge(int num){
+//         age = num;
+//     }
+// }
+
+// class oop {
+//     public static void main(String[] args) {
+//         Test test1 = new Test();
+//         test1.assignAge(num:10);
+
+//         Test test2 = new Test();
+//         test2.assignAge(19);
+
+//         System.out.println(test1.age);
+//         System.out.println(test2.age);
+//     }
+// }
+
+
+
+
+// the function printNumber which takes an integer input from the user and prints it on the screen
+class Solution{
+    public void printNumber(Scanner sc){
+        int num = sc.nextInt();
+        System.out.println(num);
     }
 }
 
-class oop {
+
+ class oop{
     public static void main(String[] args) {
-        Test test1 = new Test();
-        test1.assignAge(num:10);
-
-        Test test2 = new Test();
-        test2.assignAge(19);
-
-        System.out.println(test1.age);
-        System.out.println(test2.age);
+        Scanner sc = new Scanner(System.in);
+        Solution s1 = new Solution();
+        s1.printNumber(sc);
     }
-}
+ }
+ 

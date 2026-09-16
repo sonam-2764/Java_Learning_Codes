547a0e28c33cce43344063a741b0a4f56873f07c
Sonam Kumari <aspritualgirl@gmail.com>
2026-09-16 15:32:47 +0530

    multilevel inheritance

---
+++ oop_java/multilevel_inheritance.java
@@ -0,0 +1,24 @@
+package oop_java;
+class A{
+    void methodA(){
+        System.out.println("method of class A");
+    }
+}
+class B extends A{
+    void methodB(){
+        System.out.println("method of class B");
+    }
+}
+class C extends B{
+    void methodC(){
+        System.out.println("method of class C");
+    }
+}
+public class multilevel_inheritance {
+    public static void main(String[] args) {
+        C obj = new C();
+        obj.methodA();
+        obj.methodB();
+        obj.methodC();
+    }
+}

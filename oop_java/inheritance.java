669539e7d2b699628d00ccec0fa28668d7576046
Sonam Kumari <aspritualgirl@gmail.com>
2026-09-16 15:01:12 +0530

    inheritance

--- oop_java/inheritance.java
+++ oop_java/inheritance.java
@@ -4,6 +4,28 @@ package oop_java;
 
 //method overriding
 //reuse the code and reduce duplication 
-public class inheritance {
+//Class childClass extends parentClass
+
+// class ParentClass{
+//     void display(){
+//         System.out.println("Parent class method");
+//     }
+// }
+// class childClass extends parentClass{
+//     void show(){
+//         System.out.println("child class method");
+//     }
+// }
+// public class inheritance {
+//     public static void main(String[] args) {
+//         childClass child1 = new childClass();
+//         child1.display();//parent class method aceeseed through object of child class
+//         child1.show();//child class method
+//     }
+    
+// }
+
+
+class Vehicle{
     
 }
